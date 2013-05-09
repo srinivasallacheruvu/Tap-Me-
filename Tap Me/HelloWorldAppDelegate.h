15adeee1a0d94e660c561119a6749e99f3dd3fb4
Karthik <namburu.karthik@gmail.com>
2013-05-09 14:35:32 +0530

    Initial Commit

---
+++ Tap Me/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  Tap Me
+//
+//  Created by Karthik on 09/05/13.
+//  Copyright (c) 2013 Karthik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
